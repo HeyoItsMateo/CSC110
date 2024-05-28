67c778a9c5a055616fafb013085ab1c83569d818
matts <matts@MATTILION>
2024-05-27 19:49:40 -0700

    Added CSHero class and completed assignment

---
+++ src/CSHero.java
@@ -0,0 +1,14 @@
+
+public class CSHero {
+
+	public static void main(String[] args) {
+		System.out.println("Computer Science Hero - Matthew Flores Semyonov\n");
+		System.out.println("Name: Terry Andrew Davis");
+		System.out.println("Dates: 1969 - 2018");
+		System.out.println("Accomplishments:");
+		System.out.println("    Created the holiest of programming languages HolyC,");
+		System.out.println("    which he used to build a complete operating system TempleOS,");
+		System.out.println("    all done as a solo-developer.");
+	}
+
+}
