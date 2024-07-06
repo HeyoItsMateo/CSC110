d4401194e44ba58b8569195d6764878f40da6187
matts <matts@HP-Mattcfs>
2024-07-06 12:11:01 -0700

    Completed written assignment 6
    Added solutions to problems in written assignment 6

--- src/HW_Problems.java
+++ src/HW_Problems.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HW_Problems {
@@ -18,18 +19,56 @@ public class HW_Problems {
 			n++;
 		}
 	}
-	private static void problem_4_19_sol(int n) {
-		
+	// Problem 6.29
+	// Fill elements alternately with 0s and 1s in a checkerboard pattern
+	private static void arrayCheckerboard(int ROWS, int COLUMNS) {
+		// Initialize array according to problem specification
+		int[][] values = new int[ROWS][COLUMNS];
+		// Loop through all elements of the array
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				// Alternate checkerboard pattern using modulo
+				if (i % 2 == 0 && j % 2 == 0) {
+					values[i][j] = 1;
+				}
+				else if(i % 2 != 0 && j % 2 != 0) {
+					values[i][j] = 1;
+				}
+			}	
+		}
+		// Print array for debugging
+		for(int i = 0; i < ROWS; i++) {
+			System.out.println(Arrays.toString(values[i]));
+		}
 	}
+	// Problem 6.29
+	// Fill only the elements at the top and bottom row with zeroes
+	private static void arrayTopBottomZeroes(int ROWS, int COLUMNS) {
+		// Initialize array according to problem specification
+		int[][] values = new int[ROWS][COLUMNS];
+		// Loop through all elements of the array
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				if(i == 0 || i == ROWS - 1) {
+					values[i][j] = 0;
+				}
+				else {
+					values[i][j] = 1;
+				}
+			}	
+		}
+		for(int i = 0; i < ROWS; i++) {
+			System.out.println(Arrays.toString(values[i]));
+		}
+	}
+	// Problem-solution testing/checking
 	public static void main(String[] args) {
 		int n = 0;
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Enter integer value for n: ");
+		System.out.print("Enter integer value for Rows: ");
 		n = input.nextInt();
-		
-		problem_4_4b(n);
-		problem_4_19(n);
-		problem_4_19_sol(n);
+		System.out.print("\nEnter integer value for Columns: ");
+		arrayTopBottomZeroes(n, input.nextInt());
 	}
 }
